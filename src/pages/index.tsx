18a5865eec649ea5236a7e19182f37b45beee7d7
meli-tortelli <melissa.tortelli@gmail.com>
2023-02-23 22:23:38 -0300

    few arrangements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,9 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1 className='text-5xl font-bold'>Hello World!</h1>;
+  return (
+    <>
+      <h1 className='text-5xl font-bold'>Hello World!</h1>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
+  corePlugins: {},
 };
