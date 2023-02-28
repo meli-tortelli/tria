7d7fb74152ac43740872efd635ea289f55dbb781
meli-tortelli <melissa.tortelli@gmail.com>
2023-02-28 13:54:52 -0300

    fixed menu

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,52 +1,34 @@
 import Link from 'next/link';
 
-type LinkProps = {
-  name: string;
-  path: string;
-  subPath?: { name: string; path: string }[];
-}[];
-
 export default function Navbar() {
-  const links: LinkProps = [
-    {
-      name: 'Início',
-      path: '/',
-    },
-    {
-      name: 'Quem somos',
-      path: '/quem-somos',
-      subPath: [
-        { name: 'Desenvolvedoras', path: '/desenvolvedoras' },
-        { name: 'Nosso trabalho', path: '/nosso-trabalho' },
-      ],
-    },
-    {
-      name: 'Contato',
-      path: '/contatos',
-    },
-    {
-      name: 'Portfólio',
-      path: '/portfolio',
-    },
-  ];
-
-  // look if has submenu items
-  const menuHasSubPath = (linksArr: LinkProps) => {
-    return linksArr.filter(item => {
-      item.hasOwnProperty('subPath');
-    });
-  };
-
-  console.log(menuHasSubPath(links));
-
   return (
     <nav className='md:w-1/2 '>
       <ul className='flex items-center justify-end'>
-        {links.map((link, key) => (
-          <li key={key} className='px-3 py-2 uppercase text-sm hover:underline'>
-            <Link href={link.path}>{link.name}</Link>
-          </li>
-        ))}
+        <li className='px-3 py-2 uppercase text-sm hover:underline'>
+          <Link href='/'>Início</Link>
+        </li>
+        <li
+          className='group px-3 py-2 uppercase text-sm relative hover:underline'
+          tabIndex={0}
+        >
+          <span className='peer pointer-events-none'>Quem Somos</span>
+          <ul className='hidden group-focus-within:block peer-hover:flex transition duration-150 hover:ease-in group-hover:block hover:flex flex-col w-[200px] py-3 absolute'>
+            <li className='font-medium mb-2'>
+              <Link className='' href='/quem-somos'>
+                Desenvolvedoras
+              </Link>
+            </li>
+            <li className='font-medium'>
+              <Link href='/nosso-trabalho'>Nosso trabalho</Link>
+            </li>
+          </ul>
+        </li>
+        <li className='px-3 py-2 uppercase text-sm hover:underline'>
+          <Link href='/contato'>Contato</Link>
+        </li>
+        <li className='px-3 py-2 uppercase text-sm hover:underline'>
+          <Link href='/portfolio'>Portfólio</Link>
+        </li>
       </ul>
     </nav>
   );
