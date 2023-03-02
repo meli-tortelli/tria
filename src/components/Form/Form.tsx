a445315ce2ccbfa272458479f48419c326fa4c17
marti tortelli <m.a.illetrot@gmail.com>
2023-03-02 19:21:50 -0300

    fixed form layout

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,29 +1,29 @@
 export default function Form() {
   return (
-    <section className="h-1/2 text-center">
-      <h2 className="m-4 text-2xl font-medium my-5">Nos envie uma mensagem!</h2>
-      <h3>Estamos disponíveis para trabalhos de frella, fale com a gente para criarmos juntos.</h3>
+    <section className="h-1/2 flex flex-col justify-center">
+      <h2 className="m-4 text-2xl font-medium my-5 text-center">Nos envie uma mensagem!</h2>
+      <h3 className="mb-4 text-lg">Estamos disponíveis para trabalhos de frellancer, fale com a gente para criarmos juntos.</h3>
 
-      <form action="" className="flex flex-col w-60 self-center">
-        <div>
+      <form action="" className="flex flex-col gap-2 w-60 self-center text-lg">
+        <div className="">
           <label htmlFor="name" className="flex text-right">Nome</label>
-          <input type='text' id="name" className="form-input h-8 rounded" />
+          <input type='text' id="name" className="flex form-input h-8 w-60 rounded" />
         </div>
 
         <div>
           <label htmlFor="email" className="flex text-right">Email</label>
-          <input type='email' id="email" className="form-input h-8 rounded" />
+          <input type='email' id="email" className="flex form-input w-60 h-8 rounded" />
         </div>
 
         <div>
           <label htmlFor="projeto" className="flex text-right">Tipo de Projeto
           </label>
-          <input type='text' id="projeto" className="form-input h-8 rounded" />
+          <input type='text' id="projeto" className="flex form-input h-8 w-60 rounded" />
         </div>
 
         <div>
           <label htmlFor="descrição" className="flex text-right">Descrição do Projeto</label>
-          <textarea id="descrição" className="form-textarea resize-none rounded"
+          <textarea id="descrição" className="flex form-textarea resize-none w-60 rounded h-50"
           />
         </div>
 
