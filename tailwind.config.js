9d98265bc9bdef537d69812ce35ac5c74bdff2d7
meli-tortelli <melissa.tortelli@gmail.com>
2023-03-10 23:07:28 -0300

    add new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ['./src/**/*.tsx', './components/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['var(--font-michroma)', ...fontFamily.serif],
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
   corePlugins: {},
