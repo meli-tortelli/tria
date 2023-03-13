6819b9b381315d2cf059933d5aa8ca74556b0c2d
Illetrot <m.a.illetrot@gmail.com>
2023-03-13 20:17:25 -0300

    changed page layoult, add fonts on app

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/Form/Form";
 
 export default function ContactPage() {
   return (
-    <main className="w-full h-150 flex flex-col justify-center items-center">
+    <main className="w-auto h-auto flex justify-between items-center">
       <Form />
     </main>
 
