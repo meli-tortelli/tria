debf2581302e58a6c8a4db4615a34a9421f80b48
Illetrot <m.a.illetrot@gmail.com>
2023-03-14 16:16:04 -0300

    add font in ServiceSection, CardDevs and CardInfo

--- src/components/CardInfo/CardInfo.tsx
+++ src/components/CardInfo/CardInfo.tsx
@@ -4,9 +4,9 @@ import meliPic from "@/../public/meli.jpg"
 
 export default function CardInfo() {
   return (
-    <section className="m-8">
-      <h1 className="text-6xl space-x-1 font-serif p-5">Quem somos</h1>
-      <p className="text-lg justify-start text-justify font-mono p-4">
+    <section className="m-4">
+      <h1 className="text-6xl space-x-1 font-serif m-4">Quem somos</h1>
+      <p className="text-lg justify-start text-justify font-mono m-4 gap-4">
         A TRIA é feita por nós, Martina e Melissa.
         Estudamos e trabalhamos com desenvolvimento, programação e TI, e buscamos oferecer as melhores Soluções em Tecnologia para você e sua empresa.
         Nosso trabalho é interdisciplinar e reúne experiências profissionais, acadêmicas e nossas referências de mundo.
