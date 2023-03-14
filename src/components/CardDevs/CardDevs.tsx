dcdf4b940477326583e363a96b1509947a8dc7e1
Illetrot <m.a.illetrot@gmail.com>
2023-03-13 23:04:42 -0300

    changed layout DevCads and DevInfo

--- src/components/CardDevs/CardDevs.tsx
+++ src/components/CardDevs/CardDevs.tsx
@@ -6,22 +6,32 @@ export default function CardDevs() {
   return (
     <>
       <section className="flex flex-col">
-        <article className="flex flex-row p-3">
-          <Image src={martiPic} alt="Martina" height={400}
-            className="rounded-full p-4" />
-          <h3 className="inline-flex min-w-max self-center text-2xl font-medium">Martina Tortelli</h3>
-          <p className="p-4 self-center text-xl">
-            Sou estudante de Tecnologia da Informação, com interesses em desenvolvimento de aplicações web e desktop,  e manutenção de hardware.
-          </p>
+        <article className="flex flex-row p-4">
+          <div className="flex">
+            <Image src={martiPic} alt="Martina" height={400}
+              className="rounded-full p-4 flex-2" />
+          </div>
+
+          <div className="flex flex-wrap">
+            <h3 className="flex flex-row text-3xl font-serif font-medium">Martina Tortelli</h3>
+            <p className="flex flex-row p-4 font-mono text-xl">
+              Sou estudante de Tecnologia da Informação, com interesses em desenvolvimento de aplicações web e desktop,  e manutenção de hardware.
+            </p>
+          </div>
         </article>
 
-        <article className="flex flex-row-reverse p-6">
-          <Image src={meliPic} alt="Melissa" height={400}
-            className="rounded-full p-4" />
-          <h3 className="inline-flex min-w-max self-center text-2xl font-medium">Melissa Tortelli</h3>
-          <p className="p-4 self-center text-xl">
-            Atualmente sou estudante de Engenharia da Computação, com formação técnica em Desenvolvimento de Sistemas. Meus interesses na área são desenvolvimento web, front-end e back-end, design system
-          </p>
+        <article className="flex flex-row-reverse p-4">
+          <div className="flex">
+            <Image src={meliPic} alt="Melissa" height={400}
+              className="rounded-full p-4 flex-2" />
+          </div>
+
+          <div className="flex flex-wrap">
+            <h3 className="flex flex-row text-3xl font-serif font-medium">Melissa Tortelli</h3>
+            <p className="flex flex-row p-4 font-mono text-xl">
+              Atualmente sou estudante de Engenharia da Computação, com formação técnica em Desenvolvimento de Sistemas. Meus interesses na área são desenvolvimento web, front-end e back-end, design system
+            </p>
+          </div>
         </article>
       </section>
     </>
--- src/components/CardInfo/CardInfo.tsx
+++ src/components/CardInfo/CardInfo.tsx
@@ -5,8 +5,8 @@ import meliPic from "@/../public/meli.jpg"
 export default function CardInfo() {
   return (
     <section className="m-8">
-      <h1 className="text-3xl space-x-1">Quem somos</h1>
-      <p className="text-lg justify-start text-justify">
+      <h1 className="text-6xl space-x-1 font-serif p-5">Quem somos</h1>
+      <p className="text-lg justify-start text-justify font-mono p-4">
         A TRIA é feita por nós, Martina e Melissa.
         Estudamos e trabalhamos com desenvolvimento, programação e TI, e buscamos oferecer as melhores Soluções em Tecnologia para você e sua empresa.
         Nosso trabalho é interdisciplinar e reúne experiências profissionais, acadêmicas e nossas referências de mundo.
