24b9aac264e97863fe5e56a7ffa4d4defa226f92
marte <m.a.illetrot@gmail.com>
2023-04-05 16:04:31 -0300

    add image to homepage

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,12 +1,21 @@
 import { Button } from "../Button/button";
+import Image from "next/image";
+import image_homepage from "@/../public/image_homepage.jpg";
 
 export default function HeroSection() {
   return (
     <section className='h-screen flex flex-col items-center justify-center gap-y-10 m-4'>
-      <p className='text-3xl font-medium font-mono p-6'>A TRIA apresenta soluções tecnológicas orientadas pela sua performance e de seu negócio nas redes.</p>
+      <div>
+        <p className='text-3xl font-medium font-mono p-6'>A TRIA apresenta soluções tecnológicas orientadas pela sua performance e de seu negócio nas redes.</p>
 
-      <Button title="Saiba mais" />
+        <Button title="Saiba mais" />
 
+        <Image src={image_homepage} alt="Colegas de trabalhalo conversando" height={500} 
+        className="flex"/>
+
+      </div>
     </section>
   );
+
+  
 }
